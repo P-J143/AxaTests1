acc00f95ba7f0898e0999e318a24dda4949ca5e7
Piotr Jasniak <piotr.jasniak143@gmail.com>
2022-08-08 11:34:59 +0200

    Single browser testing

--- AxaTests1/GoogleTest.cs
+++ AxaTests1/GoogleTest.cs
@@ -4,9 +4,7 @@ using OpenQA.Selenium.Support.UI;
 using System;
 using SeleniumExtras.WaitHelpers;
 using System.Linq;
-using static AxaTests1.Browser;
-using System.Diagnostics;
-
+using OpenQA.Selenium.Chrome;
 
 namespace AxaTests1
 {
@@ -19,88 +17,83 @@ namespace AxaTests1
         private const double Distance = 30;
         private const int BicycleTime = 15;
 
-        [Theory]
-        [Browser("Firefox", HomeUrl)]
-        [Browser("Chrome", HomeUrl)]
-        public void OfficeJourney(IWebDriver driver)
+        [Fact]
+        public void OfficeJourney()
         {
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-            var decimalSeparator = ',';
-
-            // Decline cookies
-            var cookiesbutton1 = driver.FindElements(By.CssSelector(".VfPpkd-vQzf8d"));
-            var cookiesbutton2 = cookiesbutton1.FirstOrDefault(e => e.Text.Contains("Odrzuć wszystko"));
-            cookiesbutton2.Click();
-
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("#searchbox")));
-            Assert.Equal(HomeTitle, driver.Title);
-            Assert.Equal(HomeUrl, driver.Url);
-
-            // Enter the address, search for the on foot route TO the office
-            driver.FindElement(By.Id("searchboxinput")).SendKeys("Chłodna 51 Warszawa");
-            driver.FindElement(By.ClassName("hArJGc")).Click();            
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("#sb_ifc51 > input")));
-            driver.FindElement(By.CssSelector("#sb_ifc51 > input")).SendKeys("Plac Defilad 1 Warszawa");
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[aria-label='Pieszo']")));
-            driver.FindElement(By.CssSelector("[aria-label='Pieszo']")).Click();
-
-            wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("d1qxPd")));
-            var TextWalkTimeToOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
-            var TextWalkDistanceToOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
-            
-            var ResultWalkTimeToOffice = new string(TextWalkTimeToOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            var ResultWalkDistanceToOffice = new string(TextWalkDistanceToOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            Double.TryParse(ResultWalkTimeToOffice, out double NumWalkTimeToOffice);
-            Double.TryParse(ResultWalkDistanceToOffice, out double NumWalkDistanceToOffice);
-            Assert.True(WalkTime > NumWalkTimeToOffice);
-            Assert.True(Distance > NumWalkDistanceToOffice);
-
-            // Check the on foot route FROM the office 
-            driver.FindElement(By.CssSelector(".PLEQOe.reverse")).Click();
-            var TextWalkTimeFromOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
-            var TextWalkDistanceFromOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
-
-            var ResultWalkTimeFromOffice = new string(TextWalkTimeFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            var ResultWalkDistanceFromOffice = new string(TextWalkDistanceFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            Double.TryParse(ResultWalkTimeFromOffice, out double NumWalkTimeFromOffice);
-            Double.TryParse(ResultWalkDistanceFromOffice, out double NumWalkDistanceFromOffice);
-            Assert.True(WalkTime > NumWalkTimeFromOffice);
-            Assert.True(Distance > NumWalkDistanceFromOffice);
-            
-            // Check the cycling route FROM the office
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[aria-label='Na rowerze']")));
-            driver.FindElement(By.CssSelector("[aria-label='Na rowerze']")).Click();
-            var TextBicycleTimeFromOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
-            var TextBicycleDistanceFromOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
-
-            var ResultBicycleTimeFromOffice = new string(TextBicycleTimeFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            var ResultBicycleDistanceFromOffice = new string(TextBicycleDistanceFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            Double.TryParse(ResultBicycleTimeFromOffice, out double NumBicycleTimeFromOffice);
-            Double.TryParse(ResultBicycleDistanceFromOffice, out double NumBicycleDistanceFromOffice);
-            Assert.True(BicycleTime > NumBicycleTimeFromOffice);
-            Assert.True(Distance > NumBicycleDistanceFromOffice);
-
-            // Check the cycling route TO the office
-            driver.FindElement(By.CssSelector(".PLEQOe.reverse")).Click();
-            var TextBicycleTimeToOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
-            var TextBicycleDistanceToOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
-
-            var ResultBicycleTimeToOffice = new string(TextBicycleTimeFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            var ResultBicycleDistanceToOffice = new string(TextBicycleDistanceFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
-            Double.TryParse(ResultBicycleTimeToOffice, out double NumBicycleTimeToOffice);
-            Double.TryParse(ResultBicycleDistanceToOffice, out double NumBicycleDistanceToOffice);
-            Assert.True(BicycleTime > NumBicycleTimeToOffice);
-            Assert.True(Distance > NumBicycleDistanceToOffice);
-
-
-            driver.Quit();
-            foreach (var process in Process.GetProcessesByName("geckodriver"))
+            using (IWebDriver driver = new ChromeDriver())
             {
-                process.Kill();
-            }
-        }
+                driver.Navigate().GoToUrl(HomeUrl);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+                var decimalSeparator = ',';
 
+                // Decline cookies
+                var cookiesbutton1 = driver.FindElements(By.CssSelector(".VfPpkd-vQzf8d"));
+                var cookiesbutton2 = cookiesbutton1.FirstOrDefault(e => e.Text.Contains("Odrzuć wszystko"));
+                cookiesbutton2.Click();
+
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("#searchbox")));
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+
+                // Enter the address, search for the on foot route TO the office
+                driver.FindElement(By.Id("searchboxinput")).SendKeys("Chłodna 51 Warszawa");
+                driver.FindElement(By.ClassName("hArJGc")).Click();
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("#sb_ifc51 > input")));
+                driver.FindElement(By.CssSelector("#sb_ifc51 > input")).SendKeys("Plac Defilad 1 Warszawa");
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[aria-label='Pieszo']")));
+                driver.FindElement(By.CssSelector("[aria-label='Pieszo']")).Click();
+
+                wait.Until(ExpectedConditions.ElementIsVisible(By.ClassName("d1qxPd")));
+                var TextWalkTimeToOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
+                var TextWalkDistanceToOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
+
+                var ResultWalkTimeToOffice = new string(TextWalkTimeToOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                var ResultWalkDistanceToOffice = new string(TextWalkDistanceToOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                Double.TryParse(ResultWalkTimeToOffice, out double NumWalkTimeToOffice);
+                Double.TryParse(ResultWalkDistanceToOffice, out double NumWalkDistanceToOffice);
+                Assert.True(WalkTime > NumWalkTimeToOffice);
+                Assert.True(Distance > NumWalkDistanceToOffice);
+
+                // Check the on foot route FROM the office 
+                driver.FindElement(By.CssSelector(".PLEQOe.reverse")).Click();
+                var TextWalkTimeFromOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
+                var TextWalkDistanceFromOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
+
+                var ResultWalkTimeFromOffice = new string(TextWalkTimeFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                var ResultWalkDistanceFromOffice = new string(TextWalkDistanceFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                Double.TryParse(ResultWalkTimeFromOffice, out double NumWalkTimeFromOffice);
+                Double.TryParse(ResultWalkDistanceFromOffice, out double NumWalkDistanceFromOffice);
+                Assert.True(WalkTime > NumWalkTimeFromOffice);
+                Assert.True(Distance > NumWalkDistanceFromOffice);
+
+                // Check the cycling route FROM the office
+                wait.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("[aria-label='Na rowerze']")));
+                driver.FindElement(By.CssSelector("[aria-label='Na rowerze']")).Click();
+                var TextBicycleTimeFromOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
+                var TextBicycleDistanceFromOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
+
+                var ResultBicycleTimeFromOffice = new string(TextBicycleTimeFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                var ResultBicycleDistanceFromOffice = new string(TextBicycleDistanceFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                Double.TryParse(ResultBicycleTimeFromOffice, out double NumBicycleTimeFromOffice);
+                Double.TryParse(ResultBicycleDistanceFromOffice, out double NumBicycleDistanceFromOffice);
+                Assert.True(BicycleTime > NumBicycleTimeFromOffice);
+                Assert.True(Distance > NumBicycleDistanceFromOffice);
+
+                // Check the cycling route TO the office
+                driver.FindElement(By.CssSelector(".PLEQOe.reverse")).Click();
+                var TextBicycleTimeToOffice = driver.FindElement(By.CssSelector(".Fk3sm.fontHeadlineSmall")).Text;
+                var TextBicycleDistanceToOffice = driver.FindElement(By.CssSelector(".ivN21e.tUEI8e.fontBodyMedium")).Text;
+
+                var ResultBicycleTimeToOffice = new string(TextBicycleTimeFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                var ResultBicycleDistanceToOffice = new string(TextBicycleDistanceFromOffice.Where(c => char.IsDigit(c) || c == decimalSeparator).ToArray());
+                Double.TryParse(ResultBicycleTimeToOffice, out double NumBicycleTimeToOffice);
+                Double.TryParse(ResultBicycleDistanceToOffice, out double NumBicycleDistanceToOffice);
+                Assert.True(BicycleTime > NumBicycleTimeToOffice);
+                Assert.True(Distance > NumBicycleDistanceToOffice);
+            }          
+                       
+        }
 
     }
 }
--- AxaTests1/SwapiTest.cs
+++ AxaTests1/SwapiTest.cs
@@ -3,8 +3,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
 using SeleniumExtras.WaitHelpers;
-using static AxaTests1.Browser;
-using System.Diagnostics;
+using OpenQA.Selenium.Chrome;
 
 namespace AxaTests1
 {
@@ -19,32 +18,29 @@ namespace AxaTests1
 
 
 
-        [Theory]
-        [Browser("Firefox", HomeUrl)]
-        [Browser("Chrome", HomeUrl)]
-        public void TestIfLuke(IWebDriver driver)
+        [Fact]
+        public void TestIfLuke()
         {
-            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
-            Assert.Equal(HomeTitle, driver.Title);
-            Assert.Equal(HomeUrl, driver.Url);
-            driver.FindElement(By.ClassName("form-control")).Clear(); //clear the text box
-            driver.FindElement(By.ClassName("form-control")).SendKeys("people/1");
-            Assert.Contains(LukeName, driver.FindElement(By.ClassName("well")).Text); // confirm that test is looking at Luke Skywalker
-            Assert.Contains(Planet1, driver.FindElement(By.ClassName("well")).Text); // confirm that homeworld = planet1
-            Assert.Contains("https://swapi.dev/api/people/1/", driver.FindElement(By.ClassName("well")).Text); // confirm that people/1 = Luke
-            driver.FindElement(By.ClassName("form-control")).Clear(); //clear the text box
-            driver.FindElement(By.ClassName("form-control")).SendKeys("planets/1");
-            driver.FindElement(By.ClassName("input-group-btn")).Click(); // go to the homeworld
-            var Results = driver.FindElement(By.ClassName("well"));
-            wait.Until(ExpectedConditions.TextToBePresentInElement(Results, LukeHomeworld)); // wait for Tatooine to show up
-            Assert.Contains(Planet1, driver.FindElement(By.ClassName("well")).Text); // planet1 = Tatooine
-            Assert.Contains("https://swapi.dev/api/people/1/", driver.FindElement(By.ClassName("well")).Text); // residents contain people1(Luke)
-
-            driver.Quit();
-            foreach (var process in Process.GetProcessesByName("geckodriver"))
+            using (IWebDriver driver = new ChromeDriver())
             {
-                process.Kill();
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                driver.Navigate().GoToUrl(HomeUrl);
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+                driver.FindElement(By.ClassName("form-control")).Clear(); //clear the text box
+                driver.FindElement(By.ClassName("form-control")).SendKeys("people/1");
+                Assert.Contains(LukeName, driver.FindElement(By.ClassName("well")).Text); // confirm that test is looking at Luke Skywalker
+                Assert.Contains(Planet1, driver.FindElement(By.ClassName("well")).Text); // confirm that homeworld = planet1
+                Assert.Contains("https://swapi.dev/api/people/1/", driver.FindElement(By.ClassName("well")).Text); // confirm that people/1 = Luke
+                driver.FindElement(By.ClassName("form-control")).Clear(); //clear the text box
+                driver.FindElement(By.ClassName("form-control")).SendKeys("planets/1");
+                driver.FindElement(By.ClassName("input-group-btn")).Click(); // go to the homeworld
+                var Results = driver.FindElement(By.ClassName("well"));
+                wait.Until(ExpectedConditions.TextToBePresentInElement(Results, LukeHomeworld)); // wait for Tatooine to show up
+                Assert.Contains(Planet1, driver.FindElement(By.ClassName("well")).Text); // planet1 = Tatooine
+                Assert.Contains("https://swapi.dev/api/people/1/", driver.FindElement(By.ClassName("well")).Text); // residents contain people1(Luke)
             }
+
         }
     }
 
